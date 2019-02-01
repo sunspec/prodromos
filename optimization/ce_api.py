755507d1f0eb4ed8098fc4c3e980b2796328fb0d
Cliff Hansen <cwhanse@sandia.gov>
2019-02-01 12:48:47 -0700

    adjustments to ce_api.py

--- optimization/ce_api.py
+++ optimization/ce_api.py
@@ -6,6 +6,8 @@ Simple code for gathering forecasts and adjusting the power factors for multiple
 
 import requests
 from requests.auth import HTTPBasicAuth
+import json
+import pandas as pd
 
 
 class CE_API(object):
@@ -13,9 +15,11 @@ class CE_API(object):
     def __init__(self, username=None, password=None):
 
         self.proxy = {'http': 'http://wwwproxy.sandia.gov:80'}
-        self.url = "https://sandia-dm.cnrg.com/api/sandia/pf"
+        self.pf_url = "https://sandia-dm.cnrg.com/api/sandia/pf"
+        self.forecast_url = "https://sandia-dm.cnrg.com/api/pvanalysis/forecast/next"
         self.usern = username
         self.passw = password
+        self.auth = HTTPBasicAuth(self.usern, self.passw)
         self.headers = {'Content-type': 'application/json'}
 
         self.n_der = None
@@ -38,7 +42,7 @@ class CE_API(object):
         self.n_der = 0
 
         setpoints = []
-        for der_id, pf_info in self.der.iteritems():
+        for der_id, pf_info in self.der.items():
             self.n_der += 1
             # print(der, pf_info)
             setpoint = ({
@@ -59,51 +63,54 @@ class CE_API(object):
 
         print(pf_cmd)
 
-        auth = HTTPBasicAuth(self.usern, self.passw)
-        resp = requests.post(self.url, json=pf_cmd, auth=auth, proxies=self.proxy, headers=self.headers, verify=False)
+        resp = requests.post(self.pf_url, json=pf_cmd, auth=self.auth,
+                             proxies=self.proxy, headers=self.headers,
+                             verify=False)
         print('Data Posted! statusMessage: %s' % resp)
 
-    def get_forecasts(self, der):
+    def get_forecasts(self, der_ids):
         """
-        Get the forecasts of DER devices
+        Get the forecast for a DER device
 
-        :param der: dict of DER devices where the keys are the Connected Energy DER IDs, e.g.,
+        :param der_ids: list of str, names of the DER device
 
-        der = {"epri1": {'excitation': "injectingQ", 'pf': -0.95, 'forecast': None},
-               "epri2": {'excitation': "injectingQ", 'pf': -0.93, 'forecast': None},
-               "epri3": {'excitation': "injectingQ", 'pf': -0.88, 'forecast': None}}
-
-        :return: der dict with updated forecasts
+        :return forecasts: dict of pandas Series, keys are der_ids
         """
 
-        self.der = der
-
-        proxy = {'http': 'http://wwwproxy.sandia.gov:80'}
-        url = "https://sandia-dm.cnrg.com/api/pvanalysis/forecast/next"
-
-        for der_id, pf_info in self.der.iteritems():
+        forecasts = {}
+        for der_id in der_ids:
             payload = {'footprint': der_id}
             headers = {}
-            auth = HTTPBasicAuth(self.usern, self.passw)
-            resp = requests.get(url, params=payload, auth=auth, proxies=proxy, headers=headers, verify=False)
-            self.der[der_id]['forecast'] = resp.content  # add the forecast to the der dict
-
-        return self.der
+            resp = requests.get(self.forecast_url, params=payload, 
+                                auth=self.auth, proxies=self.proxy,
+                                headers=headers, verify=False)
+            points = json.loads(resp.content.decode('utf-8'))
+            idx = []
+            fc = []
+            for p in points:
+                idx.append(pd.to_datetime(p['timestamp'], utc=True))
+                fc.append(p['forecast'])
+            forecast = pd.Series(data=fc, index=idx, dtype=float)
+            forecasts[der_id] = forecast
+        return forecasts
 
 if __name__ == "__main__":
 
     username = "fake"
     password = "fake"
+    username = "sandia.api"
+    password = "5and1A"
 
     api = CE_API(username=username, password=password)
 
     der = {"epri1": {'excitation': "injectingQ", 'pf': -0.95, 'forecast': None},  # neg = Q4 (EPRI sign convention)
            "epri2": {'excitation': "injectingQ", 'pf': 0.93, 'forecast': None},
-           "epri3": {'excitation': "injectingQ", 'pf': -0.88, 'forecast': None}}
+           "epri3": {'excitation': "injectingQ", 'pf': -0.88, 'forecast': None},
+           "sunpower2201": {'excitation': "injectingQ", 'pf': 1.0, 'forecast': None}}
 
-    der = api.get_forecasts(der=der)
-    print(der)
+    fc = api.get_forecasts(der_ids=['sunpower2201'])
+    print(fc)
 
-    api.set_pf(der=der)
+#    api.set_pf(der=der)
 
 
